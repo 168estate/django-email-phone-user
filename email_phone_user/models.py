0af4203c3f648d46c78dc8893e49df7279301005
Kirill Pavlov <kirill.pavlov@phystech.edu>
2014-12-06 08:38:09 +0000

    fix import

--- email_phone_user/models.py
+++ email_phone_user/models.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.models import (
     AbstractBaseUser, BaseUserManager, PermissionsMixin,
 )
+from django.core import validators
 from django.core.mail import send_mail
 from django.db import models
 from django.utils import timezone
